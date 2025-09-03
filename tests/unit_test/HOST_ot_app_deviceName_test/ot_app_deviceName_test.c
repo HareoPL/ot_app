5320a6eecac913d0be771b7550a7000e3cc7b85b
Jan Lukaszewicz <pldevluk@gmail.com>
2025-09-03 15:19:43 +0200

    ot_devName-test: Created empty unit test

---
+++ tests/unit_test/HOST_ot_app_deviceName_test/ot_app_deviceName_test.c
@@ -0,0 +1,19 @@
+#include "unity_fixture.h"
+
+
+TEST_GROUP(ot_app_deviceName);
+
+TEST_SETUP(ot_app_deviceName)
+{
+    /* Init before every test */
+}
+
+TEST_TEAR_DOWN(ot_app_deviceName)
+{
+    /* Cleanup after every test */
+}
+
+TEST(ot_app_deviceName, test1)
+{
+    TEST_FAIL_MESSAGE("1: It is template for unit test. Use it to create your own test.");
+}
---
+++ tests/unit_test/HOST_ot_app_deviceName_test/ot_app_deviceName_test_runner.c
@@ -0,0 +1,14 @@
+#include "unity_fixture.h"
+
+TEST_GROUP_RUNNER(ot_app_deviceName)
+{
+   /* Test cases to run */
+   RUN_TEST_CASE(ot_app_deviceName, test1);
+
+}
+
+
+// TEST_GROUP_RUNNER(templateTest2)
+// {
+//    RUN_TEST_CASE(remote_rx, FirstTest);
+// }
