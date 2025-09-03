f87c2bc672c7d689f7171550e2128a2ce09c8ffe
Jan Lukaszewicz <pldevluk@gmail.com>
2025-09-03 15:19:16 +0200

    test: added cTest to the project

---
+++ tests/unit_test/ctest_list.cmake
@@ -0,0 +1,40 @@
+include(CTest)
+
+set(TEST_NAME)
+set(TEST_PATH_EXE)
+
+function(add_test_to_lists test_name)
+    list(APPEND TEST_NAME "${test_name}")
+    
+    set(test_path "${CMAKE_BINARY_DIR}/unit_test/${test_name}/${test_name}.exe")
+    
+    list(APPEND TEST_PATH_EXE "${test_path}")
+    
+    set(TEST_NAME "${TEST_NAME}" PARENT_SCOPE)
+    set(TEST_PATH_EXE "${TEST_PATH_EXE}" PARENT_SCOPE)
+endfunction()
+
+include(tests/unit_test/ctest_add_tests.cmake)
+
+list(LENGTH TEST_NAME test_name_count)
+list(LENGTH TEST_PATH_EXE test_path_count)
+
+set(added_tests "")
+
+message("TEST_NAME length=${test_name_count}")
+message("TEST_PATH_EXE length=${test_path_count}")
+
+message("TEST_NAME = ${TEST_NAME}")
+message("TEST_PATH_EXE = ${TEST_PATH_EXE}")
+
+math(EXPR last_index "${test_name_count} - 1")
+foreach(idx RANGE 0 ${last_index})
+    list(GET TEST_NAME ${idx} current_test_name)
+    list(GET TEST_PATH_EXE ${idx} current_test_path)
+
+    add_test(NAME ${current_test_name} COMMAND ${current_test_path})
+    list(APPEND added_tests ${current_test_name})
+endforeach()
+
+
+message("Tests added list: ${added_tests}")
