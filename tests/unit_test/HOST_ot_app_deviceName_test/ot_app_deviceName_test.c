e7d54262be0dc3736d73e9270a07d3221dd4dab2
Jan Lukaszewicz <pldevluk@gmail.com>
2025-09-04 08:54:18 +0200

    ot_app_devName: adde test cases for deviceNameSet()

--- tests/unit_test/HOST_ot_app_deviceName_test/ot_app_deviceName_test.c
+++ tests/unit_test/HOST_ot_app_deviceName_test/ot_app_deviceName_test.c
@@ -1,5 +1,13 @@
 #include "unity_fixture.h"
+#include "ot_app_deviceName.h"
 
+
+#define UT_DN_BAD_DEVICE_TYPE_0     OTAPP_NO_DEVICE_TYPE
+#define UT_DN_BAD_DEVICE_TYPE_MAX   OTAPP_END_OF_DEVICE_TYPE
+
+static char *deviceName_0 = {"device1"};
+static char *deviceName_max_10_byte = {"dMax10byte"};
+static char *deviceName_to_long = {"name11_byte"};
 
 TEST_GROUP(ot_app_deviceName);
 
@@ -13,7 +21,18 @@ TEST_TEAR_DOWN(ot_app_deviceName)
     /* Cleanup after every test */
 }
 
-TEST(ot_app_deviceName, test1)
+TEST(ot_app_deviceName, GivenNULLdevName_WhenIsCallingDeviceNameSet_ThenReturnError)
 {
-    TEST_FAIL_MESSAGE("1: It is template for unit test. Use it to create your own test.");
+    int8_t result;
+
+    result = otapp_deviceNameSet(NULL, OTAPP_SWITCH);
+    TEST_ASSERT_EQUAL(OTAPP_DEVICENAME_ERROR, result);
+}
+
+TEST(ot_app_deviceName, GivenIncorrectDeviceName_WhenIsCallingDeviceNameSet_ThenReturnError)
+{
+    int8_t result;
+
+    result = otapp_deviceNameSet(deviceName_0, UT_DN_BAD_DEVICE_TYPE_0);
+    TEST_ASSERT_EQUAL(OTAPP_DEVICENAME_ERROR, result);
 }
--- tests/unit_test/HOST_ot_app_deviceName_test/ot_app_deviceName_test_runner.c
+++ tests/unit_test/HOST_ot_app_deviceName_test/ot_app_deviceName_test_runner.c
@@ -2,13 +2,9 @@
 
 TEST_GROUP_RUNNER(ot_app_deviceName)
 {
-   /* Test cases to run */
-   RUN_TEST_CASE(ot_app_deviceName, test1);
+   RUN_TEST_CASE(ot_app_deviceName, GivenNULLdevName_WhenIsCallingDeviceNameSet_ThenReturnError);
+   RUN_TEST_CASE(ot_app_deviceName, GivenIncorrectDeviceName_WhenIsCallingDeviceNameSet_ThenReturnError);
+
 
 }
 
-
-// TEST_GROUP_RUNNER(templateTest2)
-// {
-//    RUN_TEST_CASE(remote_rx, FirstTest);
-// }
