fa21e989ac6ce1a27f7098a20b8738f857fd4234
Jan Lukaszewicz <pldevluk@gmail.com>
2025-09-04 09:43:14 +0200

    ot_app_devName: added more test cases for deviceNameSet()

--- tests/unit_test/HOST_ot_app_deviceName_test/ot_app_deviceName_test.c
+++ tests/unit_test/HOST_ot_app_deviceName_test/ot_app_deviceName_test.c
@@ -2,8 +2,10 @@
 #include "ot_app_deviceName.h"
 
 
-#define UT_DN_BAD_DEVICE_TYPE_0     OTAPP_NO_DEVICE_TYPE
-#define UT_DN_BAD_DEVICE_TYPE_MAX   OTAPP_END_OF_DEVICE_TYPE
+#define UT_DN_OK_DEVICE_TYPE_0                  OTAPP_CONTROL_PANEL
+
+#define UT_DN_BAD_DEVICE_TYPE_0                 OTAPP_NO_DEVICE_TYPE
+#define UT_DN_BAD_DEVICE_TYPE_OUTSIDE_SCOPE     OTAPP_END_OF_DEVICE_TYPE
 
 static char *deviceName_0 = {"device1"};
 static char *deviceName_max_10_byte = {"dMax10byte"};
@@ -25,8 +27,16 @@ TEST(ot_app_deviceName, GivenNULLdevName_WhenIsCallingDeviceNameSet_ThenReturnEr
 {
     int8_t result;
 
-    result = otapp_deviceNameSet(NULL, OTAPP_SWITCH);
+    result = otapp_deviceNameSet(NULL, UT_DN_OK_DEVICE_TYPE_0);
     TEST_ASSERT_EQUAL(OTAPP_DEVICENAME_ERROR, result);
+}
+
+TEST(ot_app_deviceName, GivenCorrectDevName_WhenIsCallingDeviceNameSet_ThenReturnError)
+{
+    int8_t result;
+
+    result = otapp_deviceNameSet(deviceName_0, UT_DN_OK_DEVICE_TYPE_0);
+    TEST_ASSERT_EQUAL(OTAPP_DEVICENAME_OK, result);
 }
 
 TEST(ot_app_deviceName, GivenIncorrectDeviceName_WhenIsCallingDeviceNameSet_ThenReturnError)
@@ -35,4 +45,36 @@ TEST(ot_app_deviceName, GivenIncorrectDeviceName_WhenIsCallingDeviceNameSet_Then
 
     result = otapp_deviceNameSet(deviceName_0, UT_DN_BAD_DEVICE_TYPE_0);
     TEST_ASSERT_EQUAL(OTAPP_DEVICENAME_ERROR, result);
+}
+
+TEST(ot_app_deviceName, GivenOutsideScopeDeviceName_WhenIsCallingDeviceNameSet_ThenReturnError)
+{
+    int8_t result;
+
+    result = otapp_deviceNameSet(deviceName_0, UT_DN_BAD_DEVICE_TYPE_OUTSIDE_SCOPE);
+    TEST_ASSERT_EQUAL(OTAPP_DEVICENAME_ERROR, result);
+}
+
+TEST(ot_app_deviceName, GivenCorrectDeviceName_WhenIsCallingDeviceNameSet_ThenReturnOK)
+{
+    int8_t result;
+
+    result = otapp_deviceNameSet(deviceName_0, UT_DN_OK_DEVICE_TYPE_0);
+    TEST_ASSERT_EQUAL(OTAPP_DEVICENAME_OK, result);
+}
+
+TEST(ot_app_deviceName, GivenTooLongDevName_WhenIsCallingDeviceNameSet_ThenReturnError)
+{
+    int8_t result;
+
+    result = otapp_deviceNameSet(deviceName_to_long, UT_DN_OK_DEVICE_TYPE_0);
+    TEST_ASSERT_EQUAL(OTAPP_DEVICENAME_TOO_LONG, result);
+}
+
+TEST(ot_app_deviceName, GivenMaxLengthDevName_WhenIsCallingDeviceNameSet_ThenReturnError)
+{
+    int8_t result;
+
+    result = otapp_deviceNameSet(deviceName_max_10_byte, UT_DN_OK_DEVICE_TYPE_0);
+    TEST_ASSERT_EQUAL(OTAPP_DEVICENAME_OK, result);
 }
--- tests/unit_test/HOST_ot_app_deviceName_test/ot_app_deviceName_test_runner.c
+++ tests/unit_test/HOST_ot_app_deviceName_test/ot_app_deviceName_test_runner.c
@@ -3,8 +3,12 @@
 TEST_GROUP_RUNNER(ot_app_deviceName)
 {
    RUN_TEST_CASE(ot_app_deviceName, GivenNULLdevName_WhenIsCallingDeviceNameSet_ThenReturnError);
+   RUN_TEST_CASE(ot_app_deviceName, GivenCorrectDevName_WhenIsCallingDeviceNameSet_ThenReturnError);
    RUN_TEST_CASE(ot_app_deviceName, GivenIncorrectDeviceName_WhenIsCallingDeviceNameSet_ThenReturnError);
-
+   RUN_TEST_CASE(ot_app_deviceName, GivenOutsideScopeDeviceName_WhenIsCallingDeviceNameSet_ThenReturnError);
+   RUN_TEST_CASE(ot_app_deviceName, GivenCorrectDeviceName_WhenIsCallingDeviceNameSet_ThenReturnOK);
+   RUN_TEST_CASE(ot_app_deviceName, GivenTooLongDevName_WhenIsCallingDeviceNameSet_ThenReturnError);
+   RUN_TEST_CASE(ot_app_deviceName, GivenMaxLengthDevName_WhenIsCallingDeviceNameSet_ThenReturnError);
 
 }
 
