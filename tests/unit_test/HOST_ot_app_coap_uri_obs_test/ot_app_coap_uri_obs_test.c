17181959f72d5861c8d524e778cfe3269c639799
Jan Lukaszewicz <pldevluk@gmail.com>
2025-09-18 12:01:48 +0200

    ot_uri_obs-test: added new test cases

--- tests/unit_test/HOST_ot_app_coap_uri_obs_test/ot_app_coap_uri_obs_test.c
+++ tests/unit_test/HOST_ot_app_coap_uri_obs_test/ot_app_coap_uri_obs_test.c
@@ -7,16 +7,29 @@
 
 #define TEST_OBS_HANDLE oac_uri_obs_getSubListHandle()
 
+void test_obs_fillTake();
+
 TEST_GROUP(ot_app_coap_uri_obs);
 
 TEST_SETUP(ot_app_coap_uri_obs)
 {
     /* Init before every test */
+    oac_uri_obs_deleteAll(TEST_OBS_HANDLE);    
 }
 
 TEST_TEAR_DOWN(ot_app_coap_uri_obs)
 {
     /* Cleanup after every test */
+}
+
+void test_obs_fillTake()
+{
+    uint8_t i = 0;
+
+    while (oac_uri_obs_spaceTake(TEST_OBS_HANDLE, i) == OAC_URI_OBS_OK)
+    {
+       i++;
+    }    
 }
 
 // spaceTake()
@@ -34,4 +47,62 @@ TEST(ot_app_coap_uri_obs, GivenOverflowIndexArg_WhenCallingSpaceTake_ThenError)
 
     result_ = oac_uri_obs_spaceTake(TEST_OBS_HANDLE, TEST_OBS_LIST_INDEX_OVERFLOW);
     TEST_ASSERT_EQUAL(OAC_URI_OBS_ERROR, result_);
+}
+
+TEST(ot_app_coap_uri_obs, GivenTrueIndexArg_WhenCallingSpaceTake_ThenOK)
+{
+    oacu_result_t result_;
+
+    result_ = oac_uri_obs_spaceTake(TEST_OBS_HANDLE, TEST_OBS_LIST_INDEX_0);
+    TEST_ASSERT_EQUAL(OAC_URI_OBS_OK, result_);
+}
+
+TEST(ot_app_coap_uri_obs, GivenMaxTrueIndexArg_WhenCallingSpaceTake_ThenOK)
+{
+    oacu_result_t result_;
+
+    result_ = oac_uri_obs_spaceTake(TEST_OBS_HANDLE, TEST_OBS_LIST_INDEX_MAX);
+    TEST_ASSERT_EQUAL(OAC_URI_OBS_OK, result_);
+}
+
+// deleteAll()
+TEST(ot_app_coap_uri_obs, GivenNULLArg_WhenCallingdeleteAll_ThenError)
+{
+    oacu_result_t result_;
+
+    result_ = oac_uri_obs_deleteAll(NULL);
+    TEST_ASSERT_EQUAL(OAC_URI_OBS_ERROR, result_);
+}
+
+TEST(ot_app_coap_uri_obs, GivenTrueArg_WhenCallingdeleteAll_ThenOk)
+{
+    oacu_result_t result_;
+
+    result_ = oac_uri_obs_deleteAll(TEST_OBS_HANDLE);
+    TEST_ASSERT_EQUAL(OAC_URI_OBS_OK, result_);
+}
+
+// spaceIsFree()
+TEST(ot_app_coap_uri_obs, GivenNullArg_WhenCallingSpaceIsFree_ThenError)
+{
+    oacu_result_t result_;
+
+    result_ = oac_uri_obs_spaceIsFree(NULL);
+    TEST_ASSERT_EQUAL(OAC_URI_OBS_ERROR, result_);
+}
+
+TEST(ot_app_coap_uri_obs, GivenTrueArg_WhenCallingSpaceIsFree_ThenOk)
+{
+    oacu_result_t result_;
+
+    result_ = oac_uri_obs_spaceIsFree(TEST_OBS_HANDLE);
+    TEST_ASSERT_EQUAL(TEST_OBS_LIST_INDEX_0, result_);
+}
+
+TEST(ot_app_coap_uri_obs, GivenFillList_WhenCallingSpaceIsFree_ThenError)
+{
+    oacu_result_t result_;
+    test_obs_fillTake();
+    result_ = oac_uri_obs_spaceIsFree(TEST_OBS_HANDLE);
+    TEST_ASSERT_EQUAL(OAC_URI_OBS_LIST_FULL, result_);
 }
--- tests/unit_test/HOST_ot_app_coap_uri_obs_test/ot_app_coap_uri_obs_test_runner.c
+++ tests/unit_test/HOST_ot_app_coap_uri_obs_test/ot_app_coap_uri_obs_test_runner.c
@@ -4,5 +4,16 @@ TEST_GROUP_RUNNER(ot_app_coap_uri_obs)
 {
    RUN_TEST_CASE(ot_app_coap_uri_obs, GivenNullArg_WhenCallingSpaceTake_ThenError);
    RUN_TEST_CASE(ot_app_coap_uri_obs, GivenOverflowIndexArg_WhenCallingSpaceTake_ThenError);
+   RUN_TEST_CASE(ot_app_coap_uri_obs, GivenTrueIndexArg_WhenCallingSpaceTake_ThenOK);
+   RUN_TEST_CASE(ot_app_coap_uri_obs, GivenMaxTrueIndexArg_WhenCallingSpaceTake_ThenOK);
+
+   // deleteAll()
+   RUN_TEST_CASE(ot_app_coap_uri_obs, GivenNULLArg_WhenCallingdeleteAll_ThenError);
+   RUN_TEST_CASE(ot_app_coap_uri_obs, GivenTrueArg_WhenCallingdeleteAll_ThenOk);
+
+   // spaceIsFree()
+   RUN_TEST_CASE(ot_app_coap_uri_obs, GivenNullArg_WhenCallingSpaceIsFree_ThenError);
+   RUN_TEST_CASE(ot_app_coap_uri_obs, GivenTrueArg_WhenCallingSpaceIsFree_ThenOk);
+   RUN_TEST_CASE(ot_app_coap_uri_obs, GivenFillList_WhenCallingSpaceIsFree_ThenError);
 
 }
