6473b61c2dbfe890dc32bc375b28c3d54e7b96c4
Jan Lukaszewicz <pldevluk@gmail.com>
2025-09-18 10:00:28 +0200

    ot_uri_obs-test: added new test

---
+++ tests/unit_test/HOST_ot_app_coap_uri_obs_test/ot_app_coap_uri_obs_test.c
@@ -0,0 +1,20 @@
+#include "unity_fixture.h"
+
+
+TEST_GROUP(ot_app_coap_uri_obs);
+
+TEST_SETUP(ot_app_coap_uri_obs)
+{
+    /* Init before every test */
+}
+
+TEST_TEAR_DOWN(ot_app_coap_uri_obs)
+{
+    /* Cleanup after every test */
+}
+
+TEST(ot_app_coap_uri_obs, test1)
+{
+    TEST_FAIL_MESSAGE("1: It is template for unit test. Use it to create your own test.");
+}
+
---
+++ tests/unit_test/HOST_ot_app_coap_uri_obs_test/ot_app_coap_uri_obs_test_runner.c
@@ -0,0 +1,13 @@
+#include "unity_fixture.h"
+
+TEST_GROUP_RUNNER(ot_app_coap_uri_obs)
+{
+   /* Test cases to run */
+   RUN_TEST_CASE(ot_app_coap_uri_obs, test1);
+}
+
+
+// TEST_GROUP_RUNNER(templateTest2)
+// {
+//    RUN_TEST_CASE(remote_rx, FirstTest);
+// }
