23116742787c1b3e2175b7f5fb4baa0345ac232c
Jan Lukaszewicz <pldevluk@gmail.com>
2025-09-18 11:12:50 +0200

    ot_uri_obs-test: added new test cases

--- tests/unit_test/HOST_ot_app_coap_uri_obs_test/ot_app_coap_uri_obs_test.c
+++ tests/unit_test/HOST_ot_app_coap_uri_obs_test/ot_app_coap_uri_obs_test.c
@@ -1,5 +1,11 @@
 #include "unity_fixture.h"
+#include "ot_app_coap_uri_obs.h"
 
+#define TEST_OBS_LIST_INDEX_0           0
+#define TEST_OBS_LIST_INDEX_MAX         (OAC_URI_OBS_SUBSCRIBERS_MAX_NUM - 1)
+#define TEST_OBS_LIST_INDEX_OVERFLOW    OAC_URI_OBS_SUBSCRIBERS_MAX_NUM
+
+#define TEST_OBS_HANDLE oac_uri_obs_getSubListHandle()
 
 TEST_GROUP(ot_app_coap_uri_obs);
 
@@ -13,8 +19,19 @@ TEST_TEAR_DOWN(ot_app_coap_uri_obs)
     /* Cleanup after every test */
 }
 
-TEST(ot_app_coap_uri_obs, test1)
+// spaceTake()
+TEST(ot_app_coap_uri_obs, GivenNullArg_WhenCallingSpaceTake_ThenError)
 {
-    TEST_FAIL_MESSAGE("1: It is template for unit test. Use it to create your own test.");
+    oacu_result_t result_;
+
+    result_ = oac_uri_obs_spaceTake(NULL, TEST_OBS_LIST_INDEX_0);
+    TEST_ASSERT_EQUAL(OAC_URI_OBS_ERROR, result_);
 }
 
+TEST(ot_app_coap_uri_obs, GivenOverflowIndexArg_WhenCallingSpaceTake_ThenError)
+{
+    oacu_result_t result_;
+
+    result_ = oac_uri_obs_spaceTake(TEST_OBS_HANDLE, TEST_OBS_LIST_INDEX_OVERFLOW);
+    TEST_ASSERT_EQUAL(OAC_URI_OBS_ERROR, result_);
+}
--- tests/unit_test/HOST_ot_app_coap_uri_obs_test/ot_app_coap_uri_obs_test_runner.c
+++ tests/unit_test/HOST_ot_app_coap_uri_obs_test/ot_app_coap_uri_obs_test_runner.c
@@ -2,12 +2,7 @@
 
 TEST_GROUP_RUNNER(ot_app_coap_uri_obs)
 {
-   /* Test cases to run */
-   RUN_TEST_CASE(ot_app_coap_uri_obs, test1);
+   RUN_TEST_CASE(ot_app_coap_uri_obs, GivenNullArg_WhenCallingSpaceTake_ThenError);
+   RUN_TEST_CASE(ot_app_coap_uri_obs, GivenOverflowIndexArg_WhenCallingSpaceTake_ThenError);
+
 }
-
-
-// TEST_GROUP_RUNNER(templateTest2)
-// {
-//    RUN_TEST_CASE(remote_rx, FirstTest);
-// }
