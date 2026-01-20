775eacf95bb984726c864514b2319a2548c3142e
Jan Łukaszewicz <plhareo@gmail.com>
2026-01-20 06:20:14 +0100

    refactor:  migrate app to separate repo and extract app files
    Use of the ot_app application for a dedicated repository for unavailable modularities.

--- tests/unit_test/ctest_list.cmake
+++ tests/unit_test/ctest_list.cmake
@@ -1,20 +1,24 @@
 include(CTest)
 
+enable_testing()
+
 set(TEST_NAME)
 set(TEST_PATH_EXE)
 
 function(add_test_to_lists test_name)
     list(APPEND TEST_NAME "${test_name}")
     
-    set(test_path "${CMAKE_BINARY_DIR}/unit_test/${test_name}/${test_name}.exe")
-    
+    # set(test_path "${CMAKE_BINARY_DIR}/unit_test/${test_name}/${test_name}.exe")
+    set(test_path "../../build/unit_test/${test_name}/${test_name}.exe")
+    # set(test_path "${CMAKE_BINARY_DIR}/unit_test/${test_name}/${test_name}${CMAKE_EXECUTABLE_SUFFIX}")
     list(APPEND TEST_PATH_EXE "${test_path}")
     
     set(TEST_NAME "${TEST_NAME}" PARENT_SCOPE)
     set(TEST_PATH_EXE "${TEST_PATH_EXE}" PARENT_SCOPE)
 endfunction()
 
-include(tests/unit_test/ctest_add_tests.cmake)
+# include(tests/unit_test/ctest_add_tests.cmake)
+include(ctest_add_tests.cmake)
 
 list(LENGTH TEST_NAME test_name_count)
 list(LENGTH TEST_PATH_EXE test_path_count)
